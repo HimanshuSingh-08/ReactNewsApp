eb1ffd1e1f3ae8098e61836dc3d0ca4d801e25d7
HimanshuSingh-08 <himansh.singh293@gmail.com>
2022-10-11 22:28:58 +0530

    Updates

---
+++ precache-manifest.00474ae146d6041f0a8130f702dd8164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8461c03c2861974bd4fd",
+    "url": "/static/css/main.83cfeb42.chunk.css"
+  },
+  {
+    "revision": "8461c03c2861974bd4fd",
+    "url": "/static/js/main.8461c03c.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "ed8fef6afb5001ac06ad",
+    "url": "/static/js/2.ed8fef6a.chunk.js"
+  },
+  {
+    "revision": "6ee34327ee87b8f52b89",
+    "url": "/static/js/runtime~main.6ee34327.js"
+  },
+  {
+    "revision": "b7e336a52550d18d2d8a1f0058d4447e",
+    "url": "/static/media/loading.b7e336a5.gif"
+  },
+  {
+    "revision": "f2a2aab28c473b4b42c583fa2ae809d4",
+    "url": "/index.html"
+  }
+];
